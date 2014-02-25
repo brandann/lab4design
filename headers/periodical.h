a4becd513f9e30033ae9702f149a7665111671ce
Kowalsky <JKowalsky@users.noreply.github.com>
2014-02-24 18:39:08 -0800

    Update periodical.h
    Added member variables and methods.

--- headers/periodical.h
+++ headers/periodical.h
@@ -4,16 +4,27 @@
 #define PERIODICAL_H
 
 #include <iostream>
-class Periodical
+class Periodical : public item
 {
 	public:
 		Periodical();
 		~Periodical();
+		string title;
+		string author;
+		string issue;
+		int year;
+		int month;
+		const char "P";  // or const char type initialized to "P"
 
 	protected:
 
 	private:
-
+		string getTitle();
+		string getAuthor();
+		string getIssue();
+		int getYear();
+		int getMonth();
+		char getType(); // always returns "P", might not need data member const char P
 };
 
 #endif // PERIODICAL_H
