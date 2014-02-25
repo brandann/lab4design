928bcab4067afafd8218584a73fe34c8e16140df
Kowalsky <JKowalsky@users.noreply.github.com>
2014-02-24 18:19:00 -0800

    Update book.h
    Added member variables and methods.

--- headers/book.h
+++ headers/book.h
@@ -3,16 +3,24 @@
 #ifndef BOOK_H
 #define BOOK_H
 
+#include <string>
+
 #include <iostream>
-class Book
+class Book : public Item
 {
 	public:
-		Book();
+		Book(string title, string author, int year);
 		~Book();
+		string getTitle():
+		string getAuthor();
+		int getYear();
 
 	protected:
 
 	private:
+		string title;
+		string author;
+		int year;
 
 };
 
