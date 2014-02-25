b3718991d58554e0df303ed8ca8e3f6c964dbb47
Brandan Haertel <foilpudding@gmail.com>
2014-02-24 19:49:35 -0800

    Update checkin.h
    added comments

--- headers/checkin.h
+++ headers/checkin.h
@@ -6,16 +6,33 @@
 #include <iostream>
 #include "transaction.h"
 
+// Checkin
+// child class holds Person and Item information involved in a single
+// Checkin action.
+// Person must be a valid pointer to a Person object
+// Item must be a valid pointer to a Item object
 class CheckIn: Public Transaction
 {
 	public:
+		// Checkin
+		// Default constructor, sets Transaction to 
+		// Person, Item
 		CheckIn(Person*, Item*):Transaction(Person*, Item*);
+		
+		// ~Checkin
+		// Default deconstructor
 		virtual ~Checkin();
+		
+		// getType
+		// returns char type "token" to identify the category
+		// of the object
 		virtual char getType() const;
 
 	protected:
 
 	private:
+		// token
+		// char identifier of the object category
 		const char token = 'R';
 
 };
