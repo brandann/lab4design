dc851b23905525d80059b7ba062d41d4282a3d62
Brandan Haertel <foilpudding@gmail.com>
2014-02-24 19:30:57 -0800

    Update transaction.h
    added comments, virtual, and pure virtuals to transaction.h

--- headers/transaction.h
+++ headers/transaction.h
@@ -7,16 +7,34 @@
 #include "person.h"
 #include "item.h"
 
+// Transaction
+// tranaction holds person and item related to eachother for a
+// transaction child class.
+// Person must be a valid pointer to a person object
+// Item must be a valid pointer to a item objcet
 class Transaction
 {
 	public:
-		Transaction();
-		virtual ~Transaction(Person*, Item*);
+		// default constructor, child classes to implement
+		// person and item stored.
+		virtual Transaction(Person*, Item*) = 0;
+		
+		// default deconstructor
+		virtual ~Transaction();
+		
+		// getType()
+		// returns the char token of the child class
+		virtual char getType() const = 0;
 
 	protected:
 
 	private:
+		// pointer to person object that is involved in
+		// this transaction
 		Person *person;
+		
+		// pointer to item object that is involved in
+		// this transaction
 		Item *item;
 
 };
