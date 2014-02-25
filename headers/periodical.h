8f1048746f857274e29a3237fa5dd915712e887f
Kowalsky <JKowalsky@users.noreply.github.com>
2014-02-24 18:41:22 -0800

    Update periodical.h

--- headers/periodical.h
+++ headers/periodical.h
@@ -9,22 +9,23 @@ class Periodical : public item
 	public:
 		Periodical();
 		~Periodical();
-		string title;
-		string author;
-		string issue;
-		int year;
-		int month;
-		const char "P";  // or const char type initialized to "P"
 
-	protected:
-
-	private:
 		string getTitle();
 		string getAuthor();
 		string getIssue();
 		int getYear();
 		int getMonth();
 		char getType(); // always returns "P", might not need data member const char P
+
+	protected:
+
+	private:
+		string title;
+		string author;
+		string issue;
+		int year;
+		int month;
+		const char "P";  // or const char type initialized to "P"
 };
 
 #endif // PERIODICAL_H
