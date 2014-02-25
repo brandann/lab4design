b09671205fd40b54db32dfb5ef59d0df3145bbea
prevnode <jgmixson@uw.edu>
2014-02-24 19:31:12 -0800

    Update hashableitem.h

--- headers/hashableitem.h
+++ headers/hashableitem.h
@@ -4,19 +4,31 @@
 //HashableItem: is an abstract class designed to be stored in a Hashtable. 
 //              Derived classes define what data to store and how to hash that data.
 
+
 #ifndef HASHABLEITEM_H
 #define HASHABLEITEM_H
 
-#include <iostream>
+
+#include <vector>                                       //vector
+#include "transaction.h"
+
 class HashableItem
 {
 	public:
-		HashableItem();
-		virtual ~HashableItem();
+		HasableItem();					//default constructor
+		virtual ~HashableItem();                        //destructor
+		virtual int hash(const int) const = 0;          //hash itself and return int between 0 and argument
+		virtual bool operator==(const HasableItem &) const = 0; //compare to another hashableitem
+		
+		vector<*Transaction> getHistory() const;	//return container of pointers to all transactions
+		void addToHistory(const *Tranasction &);        //add a transaction to history
+		void clearHistory();                            //remove all items from items history
 
 	protected:
+		
 
 	private:
+		vector<*Transaction> history;			//points to all transactions that involve this item
 
 };
 
