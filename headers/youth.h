dbf5b6d8be8a36f0b41cc7527921ceada7566a3b
Kowalsky <JKowalsky@users.noreply.github.com>
2014-02-24 19:51:04 -0800

    Update youth.h

--- headers/youth.h
+++ headers/youth.h
@@ -3,17 +3,43 @@
 #ifndef YOUTH_H
 #define YOUTH_H
 
-#include <iostream>
+#include "book.h"
+
 class Youth : public Book
 {
-	public:
-		Youth();
-		~Youth();
-		char getType(); // always returns "Y"
-	protected:
+public:
+   //--------------------------------------------------------------------------
+   // constructor
+   // Constructs an instance of Youth
+   // Assumptions: All information needed to construct the Youth Book is provided.
+   // Precondition: Youth information is supplied.
+   // Postcondition: Youth has been constructed.
+   // Sample call: Youth b("So Long and Thanks for all the Fish",
+   // "Douglas Adams", 1990);
+   Youth();
+   
+   //--------------------------------------------------------------------------
+   // destructor
+   // Destructs an instance of Youth
+   // Assumptions: Youth exists.
+   // Precondition: Youth exists.
+   // Postcondition: Youth has been destroyed..
+   // Sample call: Called automatically at end of program.
+   ~Youth();
+   
+   //--------------------------------------------------------------------------
+   // getType (Public)
+   // Returns the type of the book's publication.
+   // Assumptions: type is correct.
+   // Precondition: None.
+   // Postcondition: type is given to caller.
+   // Sample call: int type = getType();
+   char getType(); // always returns "Y"
 
-	private:
-		const char "Y";
+protected:
+
+private:
+   const char "Y";
 };
 
 #endif // YOUTH_H
