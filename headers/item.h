1555fe9b8061c52bb4686eabe1f7a8dad13126a9
Kowalsky <JKowalsky@users.noreply.github.com>
2014-02-24 18:56:34 -0800

    Update item.h
    Added Comments.

--- headers/item.h
+++ headers/item.h
@@ -1,24 +1,64 @@
 // item.h
 
+//--------------------------------------------------------------------------
+// Item
+// Designed by: Brandan Haertel, Erick House, Jennifer Kowalsky,
+//		Gary Mixson, Binh Nguyen
+// Implemented by: 
+// An item in in the library.  Base class for Books, Periodicals, ect.
+// Assumptions:   All items in the library are represented by Item and its
+//		  base classes.
+//
+
 #ifndef ITEM_H
 #define ITEM_H
 
 #include <iostream>
+
 class Item
 {
-	public:
-                Item();
-                ~Item();
-                 int getQuantity(); // return number of copies
+public:
+   //--------------------------------------------------------------------------
+   // constructor
+   // Constructs an instance of Item
+   // Assumptions:   All information needed to construct the Item is provided.
+   // Precondition:  Item information is supplied.
+   // Postcondition: Item has been constructed.
+   //		     quantity is incremented by 1.
+   Item();
+   
+   //--------------------------------------------------------------------------
+   // destructor
+   // Destructs an instance of Item
+   // Assumptions:   Item exists. Quanity is correct.
+   // Precondition:  Item exists.
+   // Postcondition: Item has been destroyed..
+   //		     quantity is decremented by 1.
+   ~Item();
+   
+   //--------------------------------------------------------------------------
+   // getQuantity (Public)
+   // Returns the number of copies of an Item.
+   // Assumptions:   Quanity is correct.
+   // Precondition:  None.
+   // Postcondition: quantity is given to caller.
+   int getQuantity();
+   
+   //--------------------------------------------------------------------------
+   // getFormat(Public)
+   // Returns the Format of an item.
+   // Assumptions:   Foramt is correct.
+   // Precondition:  None.
+   // Postcondition: Format is given to caller.
+   Format getFormat();
 
-	protected:
+protected:
 
-	private:
-	
-	        int quantity; // number of copies of the item
-                Format format; // such as hardcopy or ebook
-                // might be implemented with a hardcoded string at this point,
-                // since there is no current Format class.
+private:
+   int quantity;  // number of copies of the item
+   Format format; // such as hardcopy or ebook
+                  // might be implemented with a hardcoded string at this point,
+                  // since there is no current Format class.
   
 
 };
