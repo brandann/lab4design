c7b0c0e0418ee38237c86858c0bf87b6207498ca
Brandan Haertel <foilpudding@gmail.com>
2014-02-24 20:40:22 -0800

    Update transactionfactory.h

--- headers/transactionfactory.h
+++ headers/transactionfactory.h
@@ -6,16 +6,31 @@
 #include <iostream>
 #include "transaction.h"
 
+// TransactionFactory
+// parses and returns a clone of a Transaction item
 class TransactionFactory
 {
 	public:
+		// TransactionFactory
+		// Default constructor
 		TransactionFactory();
+		
+		// ~TransactionFactory
+		// Default deconsructor
 		~TransactionFactory();
+		
+		// build
+		// parses the string token, and creates a Transaction
+		// item with Person and Item.
+		// string must be category token
+		// Person must be valid pointer to Person object
+		// Item must be valid pointer to Item object
 		bool build(string, Person*, Item*);
 
 	protected:
 
 	private:
+		// Transaction array of Transaction children
 		Transaction *transaction;
 
 };
