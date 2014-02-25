3e63451b99189bbf9056440cf9e589fba6fca375
Kowalsky <JKowalsky@users.noreply.github.com>
2014-02-24 18:44:32 -0800

    Update youth.h

--- headers/youth.h
+++ headers/youth.h
@@ -4,16 +4,16 @@
 #define YOUTH_H
 
 #include <iostream>
-class Youth
+class Youth : public Book
 {
 	public:
 		Youth();
 		~Youth();
-
+		char getType(); // always returns "Y"
 	protected:
 
 	private:
-
+		const char "Y";
 };
 
 #endif // YOUTH_H
