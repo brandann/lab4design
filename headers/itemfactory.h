4b2a7ee2ee4c4d6fddccf58ce5bce8dbdff408a8
Kowalsky <JKowalsky@users.noreply.github.com>
2014-02-26 13:29:49 -0800

    Update itemfactory.h
    Added Comments

--- headers/itemfactory.h
+++ headers/itemfactory.h
@@ -1,5 +1,12 @@
 //itemfactory.h
 
+//--------------------------------------------------------------------------
+// ItemFactory
+// Designed by: Brandan Haertel, Erick House, Jennifer Kowalsky,
+// 	 	Gary Mixson, Binh Nguyen
+// Implemented by:
+// parses and returns a clone of a Item item
+
 #ifndef ITEMFACTORY_H
 #define ITEMFACTORY_H
 
@@ -7,15 +14,38 @@
 class ItemFactory
 {
 	public:
+		//-----------------------------------------------------------
+		// constructor
+		// contructs an instance of ItemFactory
+		// Assumptions: All information needed to construct the
+		// ItemFactory is provided.
+    		// Precondition: ItemFactory information is supplied.
+    		// Postcondition: ItemFactory has been constructed.
 		ItemFactory();
+		
+		//-----------------------------------------------------------
+		// destructor
+		// destroys the instance of ItemFactory
+		// Assumptions: ItemFactory has been constructed
+		// Precondition: None.
+		// Postcondition: ItemFactory is destroyed
+		// 		  member memory is freed.
 		~ItemFactory();
-		Item getItem(string);
+		
+		//-----------------------------------------------------------
+		// buildItem (Public)
+		// parses the string token, and creates an Item
+		// item with string.
+    		// Assumptions: parameters are correct.
+    		// Precondition: string must be category token
+    		// Postcondition: Item instance is returned
+		Item buildItem(string);
 	protected:
 
 	private:
-		Youth youth;
-		Fiction fiction;
-		Periodical periodical;
+		Youth youth;  		// mold for Youth Books
+		Fiction fiction;	// mold for Fiction Books
+		Periodical periodical;  // mold for Periodicals
 };
 
 #endif // ITEMFACTORY_H
