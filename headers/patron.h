797f06384595cb4ae02438ab6fdef784a595e0f5
Kowalsky <JKowalsky@users.noreply.github.com>
2014-02-26 13:52:59 -0800

    Update patron.h

--- headers/patron.h
+++ headers/patron.h
@@ -1,5 +1,12 @@
 //patron.h
 
+//--------------------------------------------------------------------------
+// Patron
+// Designed by: Brandan Haertel, Erick House, Jennifer Kowalsky,
+// Gary Mixson, Binh Nguyen
+// Implemented by:
+// A Patron in the library who is able to perform transactions.
+
 #ifndef PATRON_H
 #define PATRON_H
 
@@ -7,7 +14,21 @@
 
 class Patron : public Person {
 	public:
+		//-----------------------------------------------------------
+		// constructor
+		// contructs an instance of Patron
+		// Assumptions: All information needed to construct the
+		//		patron is provided.
+		// Precondition: Patron information is supplied.
+		// Postcondition: Patron has been constructed.
 		Patron(string name, int id);
+		
+		//-----------------------------------------------------------
+		// destructor
+		// Destroys an instance of Patron
+		// Assumptions: None
+		// Precondition: None
+		// Postcondition: Patron has been destroyed.
 		~Patron();
 };
 
