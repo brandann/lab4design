0cbffc73ac8609b38c6603eca2f7380735ccab38
Brandan Haertel <foilpudding@gmail.com>
2014-02-25 16:24:26 -0800

    Update checkout.h

--- headers/checkout.h
+++ headers/checkout.h
@@ -1,5 +1,15 @@
 //checkout.h
 
+//--------------------------------------------------------------------------
+// CheckOut
+// Designed by: Brandan Haertel, Erick House, Jennifer Kowalsky,
+//		Gary Mixson, Binh Nguyen
+// Implemented by: 
+// Child class holds Person and Item information involved in a single
+// Checkout action.
+// Person must be a valid pointer to a person object
+// Item must be a valid pointer to a item objcet
+
 #ifndef CHECKOUT_H
 #define CHECKOUT_H
 
@@ -9,13 +19,28 @@
 class CheckOut: Public Transaction
 {
 	public:
+		//-----------------------------------------------------------
+		// constructor
+		// contructs an instance of CheckOut
+		// Assumptions:   	All information needed to construct the
+		// 			CheckOut is provided.
+   		// Precondition:  	CheckOut information is supplied.
+   		// Postcondition: 	CheckOut has been constructed.
+		// 			person and item stored.
 		CheckOut(Person*, Item*):Transaction(Person*, Item*);
-		virtual ~Checkout();
+		
+		//--------------------------------------------------------------------------
+		// getType (Public)
+   		// Returns the type of the transaction
+   		// Assumptions: 	type is correct.
+   		// Precondition: 	None.
+   		// Postcondition:	type is given to caller.
 		virtual char getType() const;
 
 	protected:
 
 	private:
+		// char identifier of the object category
 		const char token = 'C';
 
 };
