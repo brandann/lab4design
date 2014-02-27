68252fc7afee712498d22a34ad57fcbf49d18bf2
Brandan Haertel <foilpudding@gmail.com>
2014-02-26 18:31:47 -0800

    Update output.h

--- headers/output.h
+++ headers/output.h
@@ -1,5 +1,12 @@
 //output.h
 
+//--------------------------------------------------------------------------
+// Output
+// Designed by: Brandan Haertel, Erick House, Jennifer Kowalsky,
+//		Gary Mixson, Binh Nguyen
+// Implemented by: 
+// Input class to handle data and activity delivery.
+
 #ifndef OUTPUT_H
 #define OUTPUT_H
 
@@ -7,8 +14,29 @@
 class Output
 {
 	public:
+		//-----------------------------------------------------------
+		// constructor
+		// contructs an instance of Output
+		// Assumptions:   	None
+   		// Precondition:  	None
+   		// Postcondition: 	Output instance is created
 		Output();
+		
+		//-----------------------------------------------------------
+		// destructor
+		// Destructs an istance if Output
+		// Assumptions:		None
+		// Precondition:	Output exists
+		// Postcondition:	Output has been destroyed
 		~Output();
+		
+		//-----------------------------------------------------------
+		// display (Public)
+   		// displays the content of the library
+   		// Assumptions: 	Library is valid
+   		// Precondition: 	string is a valid output file
+   		// Postcondition:	Library is outputed to string file
+		bool display(string*);
 
 	protected:
 
